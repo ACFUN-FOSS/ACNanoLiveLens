2109fff2f8e4a9d6edbf0bf8f3594c5d876163fe
Chih-hao Su <chihhao_su@outlook.com>
2026-07-09 17:27:39 +0800

    修復 release 構建下 quickjs 在 MSVC 下編譯問題

--- subprojects/Core/public/Core/assert.hxx
+++ subprojects/Core/public/Core/assert.hxx
@@ -3,15 +3,13 @@
 
 [[noreturn]] void assertHandler(const char *expr, const char *file, int line);
 
-#ifndef NDEBUG
+
 #define NLS_ASSERT(expr) \
 	do { \
 		if (!(expr)) { \
 			::assertHandler(#expr, __FILE__, __LINE__); \
 		} \
 	} while (false)
-#else
-#define NLS_ASSERT(expr) static_cast<void>(0)
-#endif
+
 
 #endif // NANOLIVELENS_CORE_ASSERT_HXX
